3481580ed85c6ef64588a243a2586178c003ead1
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-24 18:55:04 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,28 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image,:writings, :price,:category_id, :condition_id, :shipping_charges_id, :prefecture_id, :delivery_days_id,:user_id)
+    .merge(user_id: current_user.id)
   end
 end
