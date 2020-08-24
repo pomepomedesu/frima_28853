296a65ada509244c4378c8656df46f2f31af19b0
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-24 18:53:07 +0900

    バリデーション修正(2)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :sipping_charge, :prefecture, :delivery_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_day
+
 
   with_options presence: true do
     VALID_PRICE_REGEX = /\A[a-zA-Z0-9]+\z/
@@ -12,7 +17,7 @@ class Item < ApplicationRecord
       with_options numericality: { other_than: 1 } do
         validates :category_id
         validates :condition_id
-        validates :sipping_charges_id
+        validates :shipping_charges_id
         validates :prefecture_id
         validates :delivery_days_id
       end
