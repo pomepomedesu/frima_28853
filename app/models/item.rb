69144c403529a0a5754045691864eae91a619e03
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-24 16:26:38 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,23 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :sipping_charge, :prefecture, :delivery_day
+
   with_options presence: true do
     VALID_PRICE_REGEX = /\A[a-zA-Z0-9]+\z/
     validates :image
     validates :name,               length: {maximum: 40}
     validates :writings,           length: {maximum: 1000}
-    validates :price,              format: {with: VALID_PRICE_REGEX}
+    validates :price,              format: {with: VALID_PRICE_REGEX }
     validates :user_id
-    validates :category_id
-    validates :condition_id
-    validates :sipping_charges_id
-    validates :prefecture_id
-    validates :delivery_days_id
+      with_options numericality: { other_than: 1 } do
+        validates :category_id
+        validates :condition_id
+        validates :sipping_charges_id
+        validates :prefecture_id
+        validates :delivery_days_id
+      end
   end
+
+  belongs_to :user
+  has_one_attached :image
 end
