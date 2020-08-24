709d44099988380964e17e7fc83e6671e2294866
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-24 11:18:31 +0900

    Itemテーブルのカラム追加

---
+++ db/migrate/20200824014608_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user,               null: false,  foreign_key: true
+      t.string     :name,               null: false
+      t.text       :writings,           null: false
+      t.integer    :category_id,        null: false,  foreign_key: true
+      t.integer    :condition_id,       null: false,  foreign_key: true
+      t.integer    :sipping_charges_id, null: false,  foreign_key: true
+      t.integer    :prefecture_id,      null: false,  foreign_key: true
+      t.integer    :delivery_days_id,   null: false,  foreign_key: true
+      t.integer    :price,              null: false
+      t.timestamps
+    end
+  end
+end
