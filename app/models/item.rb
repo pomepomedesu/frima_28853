37db302776609c80bdc900011259ef1164cdab8b
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-26 17:38:31 +0900

    items.rbのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,6 @@ class Item < ApplicationRecord
     validates :name,               length: {maximum: 40}
     validates :writings,           length: {maximum: 1000}
     validates :price,              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: VALID_PRICE_REGEX }
-    # validates :user_id
       with_options numericality: { other_than: 1 } do
         validates :category_id
         validates :condition_id
