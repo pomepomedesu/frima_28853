4a306de7792b854c4751f0e594c64a6e16f7a625
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-26 12:09:06 +0900

    手数料計算のJS

---
+++ app/javascript/price_caluculation.js
@@ -0,0 +1,20 @@
+ function caluculation (){
+
+  const item_price = document.getElementById("item-price");
+  const add_tax_price = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  item_price.addEventListener("input", function() {
+    const item = item_price.value;
+    console.log(item)
+    const fee = Math.floor(item * 0.1);
+    const profit_price = (item - fee);
+    add_tax_price.textContent = fee
+    profit.textContent = profit_price
+  });
+}
+window.addEventListener('load', caluculation)
+
+
+
+
+
