a1dead0ec0bc39b03825b1e6c6d12a9bfcca348e
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-28 01:11:41 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,12 +40,7 @@ class ItemsController < ApplicationController
       render 'show'
     end
   end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  
   private
 
   def item_params
@@ -55,5 +50,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
