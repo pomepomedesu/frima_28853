261164f0bfff66931920e7dd1b2d84c3743c2686
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-27 21:54:28 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,12 +22,26 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @item = Item.find(params[:id])
     if @item.destroy
       redirect_to root_path
     else
       render 'show'
+    end
   end
 
   def move_to_index
