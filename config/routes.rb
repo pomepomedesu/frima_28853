a4ea39434350375a9d39ff34641ea42504fee0b6
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-28 16:19:18 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :transactions
+  resources :items do
+    resources :transactions
+  end
 end
