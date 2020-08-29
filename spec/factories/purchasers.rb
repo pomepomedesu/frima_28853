985747110a0622e9ebe13b6da111943eea2efb22
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-29 13:09:19 +0900

    テストコード

---
+++ spec/factories/purchasers.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :purchaser do
+    token         {'params[:token]'}
+    user_id       {'1'}
+    item_id       {'1'}
+    buyer_id      {'1'}
+    postal_code   {'333-0803'}
+    prefecture_id {'2'}
+    city          {'埼玉'}
+    house_number  {'56-1'}
+    phone_number  {'08070639085'}
+  end
+end
