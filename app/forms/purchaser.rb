1e340c45249a95b3be0d5ee83ef2a704a2ca93e4
pomepomedesu <kaitopoohlove@gmail.com>
2020-08-29 13:05:41 +0900

    formオブジェクトのバリデーション修正

--- app/forms/purchaser.rb
+++ app/forms/purchaser.rb
@@ -1,25 +1,25 @@
 class Purchaser
 
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id,:buyer_id, :postal_code, :prefecture_id, :city, :house_number, :building_name,:phone_number
-
-  
+  attr_accessor :user_id,:item_id,:buyer_id, :postal_code, :prefecture_id, :city, :house_number, :building_name,:phone_number, :token
 
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
   VALID_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :buyer_id
-    validates :postal_code,  format: {with: VALID_CODE_REGEX, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :postal_code,   format: {with: VALID_CODE_REGEX, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number,   format: { with: VALID_PHONE_REGEX }
+    validates :phone_number,  format: { with: VALID_PHONE_REGEX }, length: { maximum: 11, message: "is invalid."}
   end
 
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name,buyer_id: buyer.id, phone_number: phone_number)
   end
+
 end
